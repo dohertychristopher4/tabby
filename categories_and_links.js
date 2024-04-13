5139dac8f22fc2bb6b95cad11da0abab1e4fcc63
christopherdoherty <christopher.doherty@druva.com>
2024-04-13 15:47:32 +0100

    initial commit

---
+++ categories_and_links.js
@@ -0,0 +1,48 @@
+// Data for categories and links
+let categoriesData = []
+fetchCategories = fetch('assets/categories_and_links.json')
+.then(response => response.json())
+.then(jsonData => {
+    categoriesData = jsonData
+    generateCategories()
+})
+
+fetchCategories.then(() => {
+    console.log('loading', categoriesData)
+})
+
+// Function to generate links for a category
+function generateLinks(linksData) {
+    const linksContainer = document.createElement("div")
+    linksContainer.classList.add("links")
+
+    linksData.forEach(link => {
+        const linkElement = document.createElement("a")
+        linkElement.textContent = link.text
+        linkElement.href = link.url
+        linkElement.classList.add("link")
+        linksContainer.appendChild(linkElement)
+    })
+
+    return linksContainer
+}
+
+// Function to generate categories
+function generateCategories() {
+    const gridContainer = document.querySelector(".grid-container")
+
+    categoriesData.forEach(categoryData => {
+        const categoryElement = document.createElement("div")
+        categoryElement.classList.add("category")
+
+        const categoryNameElement = document.createElement("h2")
+        categoryNameElement.textContent = categoryData.category
+        categoryElement.appendChild(categoryNameElement)
+
+        const linksContainer = generateLinks(categoryData.links)
+        categoryElement.appendChild(linksContainer)
+
+        gridContainer.appendChild(categoryElement)
+    })
+}
+
